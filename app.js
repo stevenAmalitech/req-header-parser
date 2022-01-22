c537a49a06b15953de0459e60873c2b4df9884f6
Steven Yirenkyi <yirenkyisteven7@gmail.com>
2022-01-22 14:24:57 +0000

    send req details to client

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.send("ok");
 });
 
+app.get("/api/whoami", (req, res) => {
+  const { ip: ipaddress, headers } = req;
+  const language = headers["accept-language"];
+  const software = headers["user-agent"];
+
+  res.send({ ipaddress, language, software });
+});
+
 app.listen(4000, () => {
   console.log("server up");
 });
